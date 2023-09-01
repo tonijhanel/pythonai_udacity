db8dcdf6377ac4e732a56b44017ff226c093545e
Toni Williams <toniw@adobe.com>
2023-08-31 22:03:22 -0400

    adding class files

---
+++ readdognames.py
@@ -0,0 +1,29 @@
+dognames_dic = dict()
+with open("dognames.txt", "r") as infile:
+        # Reads in dognames from first line in file
+        line = infile.readline()
+
+        # Processes each line in file until reaching EOF (end-of-file) by 
+        # processing line and adding dognames to dognames_dic with while loop
+        while line != "":
+
+            # Process line by striping newline from line
+            line = line.rstrip()
+            
+            # adds dogname(line) to dogsnames_dic if it doesn't already exist 
+            # in the dogsnames_dic dictionary
+            if line not in dognames_dic:
+                dognames_dic[line] = 1
+
+            # Reads in next line in file to be processed with while loop
+            # if this line isn't empty (EOF)
+            line = infile.readline()
+
+    # Add to whether pet labels & classifier labels are dogs by appending
+    # two items to end of value(List) in results_dic. 
+    # List Index 3 = whether(1) or not(0) Pet Image Label is a dog AND 
+    # List Index 4 = whether(1) or not(0) Classifier Label is a dog
+    # How - iterate through results_dic if labels are found in dognames_dic
+    # then label "is a dog" index3/4=1 otherwise index3/4=0 "not a dog"
+
+print (dognames_dic)
